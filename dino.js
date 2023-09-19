94c1d95c4b43b02977a25d0687bba4b76b2ed00d
NASA <80649734+Belle-The-Psycho@users.noreply.github.com>
2023-09-19 17:07:14 +1200

    Imported images for javascript

--- dino.js
+++ dino.js
@@ -24,4 +24,9 @@ window.onload = function() {
     board = document.getElementById("board");
     board.height = boardHeight;
     board.width = boardWidth;
+
+    context.board.getContext("2d"); //used for drawing on the board
+
+    dinoImg = new Image();
+    dinoImg.src = ""
 }
