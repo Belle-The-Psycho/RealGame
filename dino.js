8ee5191bcee35ee0414f3f4b12b25edd9b7179b2
NASA <80649734+Belle-The-Psycho@users.noreply.github.com>
2023-09-19 16:49:18 +1200

    dino variables

---
+++ dino.js
@@ -0,0 +1,20 @@
+
+//board
+let board;
+let boardWidth = 750;
+let boardHeight = 250;
+let context; 
+
+//dinos
+let dinoWidth = 88;
+let dinoHeight = 94;
+let dinoX = 50;
+let dinoY = dinoHeight - dinoHeight;
+let dinoImg;
+
+let dino = {
+    x : dinoX,
+    y : dinoY,
+    width : dinoWidth, 
+    height : dinoHeight
+}
